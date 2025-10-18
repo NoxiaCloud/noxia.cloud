88200a11f6b3869267bcab0a4c686ff898951c13
emi <emiray@noxia.cloud>
2025-10-18 12:10:20 +0100

    UI updates

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_minify import Minify
 from werkzeug.exceptions import HTTPException
 import logging
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 Minify(app=app, html=True, js=True, cssless=True, static=True)
@@ -35,6 +37,19 @@ def handle_errors(e):
 @app.route("/")
 def index():
     return render_template("/pages/index.html")
+
+
+@app.route("/api/status")
+def status():
+    url = "https://status.noxia.cloud/badge?theme=light"
+    r = requests.get(url)
+    html = r.text
+
+    soup = BeautifulSoup(html, "html.parser")
+    status_div = soup.find("div", class_="font-medium")
+    status_text = status_div.get_text(strip=True) if status_div else "Unknown"
+
+    return jsonify({"status": status_text})
 
 
 app.run(host="127.0.0.1", port=5000, debug=True)
